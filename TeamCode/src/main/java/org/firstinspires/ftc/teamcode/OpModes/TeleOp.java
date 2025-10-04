4bc86fa78543e880707dc9f590466ef81248c00a
Makhkambek <okeydokeykokki@gmail.com>
2025-10-04 15:37:46 +0500

    bb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -5,14 +5,19 @@ import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Controllers.BoxArmController;
+import org.firstinspires.ftc.teamcode.Controllers.ClutchController;
 import org.firstinspires.ftc.teamcode.Controllers.PTOController;
 import org.firstinspires.ftc.teamcode.Controllers.ArmIntakeController;
 import org.firstinspires.ftc.teamcode.Controllers.ResetController;
+import org.firstinspires.ftc.teamcode.Controllers.VisionController;
 import org.firstinspires.ftc.teamcode.SubSystems.BoxArm;
 import org.firstinspires.ftc.teamcode.SubSystems.Clutch;
 import org.firstinspires.ftc.teamcode.SubSystems.PTO;
 import org.firstinspires.ftc.teamcode.SubSystems.ArmIntake;
 import org.firstinspires.ftc.teamcode.SubSystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.SubSystems.Vision;
 
 @Config
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp")
@@ -24,12 +29,16 @@ public class TeleOp extends LinearOpMode {
         BoxArm boxArm = new BoxArm(hardwareMap);
         Clutch clutch = new Clutch(hardwareMap);
         DriveTrain driveTrain = new DriveTrain(hardwareMap);
+        Vision vision = new Vision(hardwareMap);
 
         GamepadEx gamepad1Ex = new GamepadEx(gamepad1);
         GamepadEx gamepad2Ex = new GamepadEx(gamepad2);
 
         PTOController ptoController = new PTOController(pto, gamepad2Ex);
+        VisionController visionController = new VisionController(vision, telemetry, gamepad1);
         ArmIntakeController armIntakeController = new ArmIntakeController(armIntake, gamepad2Ex);
+        BoxArmController boxArmController = new BoxArmController(boxArm, gamepad2Ex);
+        ClutchController clutchController = new ClutchController(clutch, gamepad2Ex);
         ResetController resetController = new ResetController(pto, armIntake, boxArm, clutch,  gamepad2Ex);
 
         boolean wasRightTriggerPressed = false;
@@ -41,6 +50,9 @@ public class TeleOp extends LinearOpMode {
 
             ptoController.update();
             armIntakeController.update();
+            clutchController.update();
+            boxArmController.update();
+//            visionController.update();
             resetController.update();
 
             boolean isRightTriggerPressed = gamepad1Ex.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.2;
